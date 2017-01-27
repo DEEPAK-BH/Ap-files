8521e90dd60561f05192b80fa2f0ad68eebfd8c3
Ishwor <ishworkh@gmail.com>
2017-01-28 00:39:50 +0200

    make selected video play in main screen

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,32 @@
 (function($){
+
+  $('.video.col').on('click', function(){
+      var url = $(this).data('youtube');
+      url += '?autoplay=1';
+      var screen = $('#video.screen');
+      var iframe = $(screen).find('iframe');
+
+      if ($(iframe).attr('src') === url)
+      {
+          _scrollToScreen();
+          return;
+      }
+
+      $(iframe).attr('src', url);
+      $(iframe).on('load', function(){
+          _scrollToScreen();
+      });
+
+      function _scrollToScreen()
+      {
+        $('html, body').animate({
+          scrollTop: $(screen).offset().top - 50
+        }, 100);
+      }
+  });
+
+
+
   $(window).scroll(function(){
       var elem = $("#banner");
       var video = $(elem).find("video")[0];
