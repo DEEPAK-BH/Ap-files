04172eeb014e487bbb5f915ca6dee37c6bf4878b
Deepak Bhandari <sunnydayfinland@gmail.com>
2017-02-05 22:00:16 +0200

    overlay added and some html and css updates were made

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -121,4 +121,22 @@
           return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
       }
     }
+
+    /* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
 })(jQuery);
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
